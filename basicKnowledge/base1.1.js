e95e4d037a3527783d09be87b4b43519bb33b492
shouwei <shouwei@staff.weibo.com>
2017-07-09 18:26:50 +0800

    项目理解

---
+++ basicKnowledge/base1.1.js
@@ -0,0 +1,92 @@
+var canvas = document.getElementById("canvas2"),
+	context = canvas.getContext("2d"),
+	radius = canvas.width/2 - 30;
+
+function drawCircle(){
+	context.beginPath();
+	context.arc(canvas.width/2, canvas.height/2, radius, 0 , Math.PI*2, true);
+	context.stroke();
+}
+
+function drawCenter(){
+	context.beginPath();
+	context.arc(canvas.width/2, canvas.height/2, 5, 0, Math.PI*2, true);
+	context.fillStyle = "#000";
+	context.fill();
+}
+
+function drawInterval(){
+	var beginPoint = {x: 0, y: 0},
+		endPoint = {x: 0, y:0};
+
+	for (var i = 0; i < 60; i++) {
+		var angle = Math.PI*2/60 * i - Math.PI/2,
+			interval = 5;
+		if (i % 5 == 0) {interval = 10};
+		beginPoint.x = canvas.width/2 + Math.cos(angle)*radius;
+		beginPoint.y = canvas.height/2 + Math.sin(angle)*radius;
+		endPoint.x = canvas.width/2 + Math.cos(angle)*(radius-interval);
+		endPoint.y = canvas.height/2 + Math.sin(angle)*(radius-interval);
+
+		context.beginPath();
+		context.moveTo(beginPoint.x, beginPoint.y);
+		context.lineTo(endPoint.x, endPoint.y);
+		context.stroke();
+	}
+
+}
+
+function drawNum(){
+	var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+	var numSpacing = 20;
+	var font = 15;
+	context.font = font+"px";
+	for (var i = 0; i < nums.length; i++) {
+		var angle = Math.PI*2/12 * (nums[i] - 3);
+		var numWidth = context.measureText(nums[i]).width;
+		context.fillText(nums[i], 
+			canvas.width/2 + Math.cos(angle)*(radius+numSpacing) - numWidth/2,
+			canvas.height/2 + Math.sin(angle)*(radius+numSpacing) + font/3);
+	}
+}
+
+function drawHand(loc, isHour){
+	var angle = Math.PI*2/60 * loc - Math.PI/2;
+	var handEnd = {x: 0, y:0};
+	var spaceLen = 15;
+	if (isHour) {
+		spaceLen = 45;
+	}
+	handEnd.x = canvas.width/2 + Math.cos(angle) * (radius - spaceLen);
+	handEnd.y = canvas.height/2 + Math.sin(angle) * (radius - spaceLen);
+	context.moveTo(canvas.width/2, canvas.height/2);
+	context.lineTo(handEnd.x, handEnd.y);
+	context.stroke();
+}
+
+function drawHands(){
+	var date = new Date();
+	var hour = date.getHours();
+	// hour = hour > 12 ? hour - 12 : hour;
+	hour = hour % 12;
+	var minute = date.getMinutes();
+	var second = date.getSeconds();
+
+	minute = minute + second/60;
+	hour = hour + minute/60;
+
+	drawHand(hour*5, true);
+	drawHand(minute, false);
+	drawHand(second, false);
+}
+
+
+function drawClock(){
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	drawCircle();
+	drawCenter();
+	drawInterval();
+	drawNum();
+	drawHands()
+}
+setInterval(drawClock, 1000);
--- basicKnowledge/base1.js
+++ basicKnowledge/base1.js
@@ -13,7 +13,7 @@ var canvas2 = document.getElementById("canvas2"),
 	margin = 35,
 	hand_truncation = canvas2.width/25,
 	hour_head_truncation = canvas2.width/10,
-	numeral_spacing = 20,
+	numeral_spacing = 20, // 数字距离时钟的距离
 	radius = canvas2.width/2 - margin,// 时钟半径
 	hand_radius = radius + numeral_spacing;
 
@@ -29,8 +29,9 @@ function drawNumerals() {
 		numeralWidth = 0;
 
 	numerals.forEach(function(numeral){
-		angle = Math.PI/6 * (numeral-3);
-		numeralWidth = context2.measureText(numeral).width;
+		angle = Math.PI*2/12 * (numeral-3); // 0~2*Math.PI  单位角度：Math.PI/6  0在3点处
+		numeralWidth = context2.measureText(numeral).width; // 测量字体的宽度
+		
 		context2.fillText(numeral, 
 			canvas2.width/2 + Math.cos(angle)*(hand_radius) - numeralWidth/2,
 			canvas2.height/2 + Math.sin(angle)*(hand_radius) + font_height/3);
@@ -45,7 +46,7 @@ function drawCenter(){
 }
 
 function drawHand(loc, isHour){
-	var angle = (Math.PI*2) * (loc/60) - Math.PI/2,
+	var angle = (Math.PI*2) * (loc/60) - Math.PI/2, // 3点时angle=0，时钟60等分
 		handRadius = isHour ? radius - hand_truncation - hour_head_truncation
 			: radius - hand_truncation;
 
@@ -60,16 +61,44 @@ function drawHands(){
 		hour = date.getHours();
 	
 	hour = hour > 12 ? hour-12 : hour;
-	
-	drawHand(hour*5 + (date.getMinutes()/60)*5, true, 0.5);
-	drawHand(date.getMinutes(), false, 0.5);
+
+	drawHand(hour*5 + (date.getMinutes()/60)*5, true, 0.5);// *5 满足60等分
+	drawHand(date.getMinutes() + (date.getSeconds()/60), false, 0.5);
 	drawHand(date.getSeconds(), false, 0.2)
+}
+// 绘制刻度
+function drawInterval(){
+	var angle = 0;
+	var beginPoint = {
+		x: 0,
+		y: 0
+	};
+	var endPoint = {
+		x: 0,
+		y: 0
+	};
+	for (var i = 0; i < 60; i++) {
+		var intervalLen = 5;
+		if (i % 5 == 0) {
+			intervalLen = 10;
+		}
+		angle = Math.PI*2/60 * (i- 15);
+		beginPoint.x = canvas2.width/2 + Math.cos(angle)*radius;
+		beginPoint.y = canvas2.height/2 +Math.sin(angle)*radius;
+		endPoint.x = canvas2.width/2 + Math.cos(angle)*(radius-intervalLen);
+		endPoint.y = canvas2.height/2 +Math.sin(angle)*(radius-intervalLen);
+		context2.moveTo(beginPoint.x, beginPoint.y);
+		context2.lineTo(endPoint.x, endPoint.y);
+		context2.stroke();
+	}
+
 }
 
 function drawClock(){
 	context2.clearRect(0, 0, canvas2.width, canvas2.height);
 
 	drawCircle();
+	drawInterval();
 	drawCenter();
 	drawHands();
 	drawNumerals();
